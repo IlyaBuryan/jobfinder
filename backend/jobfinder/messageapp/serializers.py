2b5a71fefeb6e6be3496e4cfd4b958d7937cbe47
Beleaev Mikhail <ivzh.ehb@Yandex.ru>
2022-02-05 23:52:46 +0300

    Commit message_third

--- backend/jobfinder/messageapp/serializers.py
+++ backend/jobfinder/messageapp/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 from .models import Message
 
 
-class MessageModelSerializer(serializers.HyperlinkedModelSerializer):
+class MessageModelSerializer(ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
--- backend/jobfinder/messageapp/views.py
+++ backend/jobfinder/messageapp/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Message
 from .serializers import MessageModelSerializer
 
 
-class MessageModelViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all().order_by()
+class MessageModelViewSet(ModelViewSet):
+    queryset = Message.objects.all()
     serializer_class = MessageModelSerializer
 
 
