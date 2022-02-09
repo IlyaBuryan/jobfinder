7427ee8f7e98d11d69a457be8ee68bd846396a1d
lightstyle77 <lightstyle54@mail.ru>
2022-02-09 20:53:55 +0300

    добавлены методы, фильтрация, внесены корректировки

--- backend/jobfinder/workerapp/models.py
+++ backend/jobfinder/workerapp/models.py
@@ -9,15 +9,8 @@ class Worker(models.Model):
     last_name = models.CharField(max_length=100, verbose_name='Фамилия')
     phone = models.BigIntegerField(verbose_name='Телефон')
     birth_date = models.DateField(verbose_name='Дата Рождения')
-    user = models.OneToOneField(CustomUser, verbose_name='Пользователь', on_delete=models.CASCADE, related_name='worker')
-
-
-class WorkExperience(models.Model):
-    organization = models.CharField(verbose_name='организация', max_length=128)
-    start = models.DateField(verbose_name='начало работы')
-    end = models.DateField(verbose_name='окончание работы')
-    position = models.CharField(verbose_name='должность', max_length=64)
-    functions = models.TextField(verbose_name='обязанности', blank=True)
+    user = models.OneToOneField(CustomUser, verbose_name='Пользователь', on_delete=models.CASCADE,
+                                related_name='worker')
 
 
 class Resume(models.Model):
@@ -30,7 +23,15 @@ class Resume(models.Model):
     education_types = models.IntegerField(verbose_name='Образование', choices=types)
     institution = models.CharField(max_length=100, verbose_name='Учебное заведение')
     courses = models.CharField(max_length=100, verbose_name='Курсы')
-    work_experience = models.ForeignKey(WorkExperience, verbose_name="Опыт работы", on_delete=models.CASCADE)
     info = models.TextField(verbose_name='Дополнительная информация')
     is_draft = models.BooleanField(default="false", verbose_name='Черновик')
-    worker_id = models.ForeignKey(Worker, verbose_name="Сотрудник", on_delete=models.CASCADE)
+    worker = models.ForeignKey(Worker, verbose_name="Сотрудник", on_delete=models.CASCADE)
+
+
+class WorkExperience(models.Model):
+    organization = models.CharField(verbose_name='организация', max_length=128)
+    start = models.DateField(verbose_name='начало работы')
+    end = models.DateField(verbose_name='окончание работы')
+    position = models.CharField(verbose_name='должность', max_length=64)
+    functions = models.TextField(verbose_name='обязанности', blank=True)
+    resume = models.ManyToManyField(Resume, verbose_name="Опыт работы", related_name="experience")
--- backend/jobfinder/workerapp/views.py
+++ backend/jobfinder/workerapp/views.py
@@ -8,6 +8,16 @@ class WorkerModelViewSet(viewsets.ModelViewSet):
     queryset = Worker.objects.all()
     serializer_class = WorkerModelSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        return Worker.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class ResumeModelViewSet(viewsets.ModelViewSet):
     queryset = Resume.objects.all()
