f098ef39dd99ab15b262fe48bae77d3a0d49cfae
Люция <lvaleeva87@gmail.com>
2022-02-24 09:48:26 +0500

    добавлены тесты

--- backend/jobfinder/authapp/tests.py
+++ backend/jobfinder/authapp/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
+
+from .models import CustomUser
+from .views import CustomUserModelViewSet
+
+
+class TestCustomUserViewSet(TestCase):
+    url = '/api/v1/user/'
+
+    def setUp(self) -> None:
+        self.admin = CustomUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        self.user = CustomUser.objects.create(username='test_user', email='test@mail.ru', password='qazwsx', role=2)
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        force_authenticate(request, self.admin)
+        view = CustomUserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_admin(self):
+        factory = APIRequestFactory()
+        request = factory.post(self.url, {'username': 'test_user_x', 'password': 'qazwsx123',
+                                          'email': 'mail222@mail.ru', 'role': 2}, format='json')
+        force_authenticate(request, self.admin)
+        view = CustomUserModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_detail(self):
+        client = APIClient()
+        response = client.get(f'{self.url}{self.user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def tearDown(self) -> None:
+        pass
--- backend/jobfinder/companyapp/tests.py
+++ backend/jobfinder/companyapp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APITestCase
+
+from companyapp.views import CompanyCardModelViewSet
+
+from authapp.models import CustomUser
+
+
+class TestCompanyCardViewSet(TestCase):
+    url = '/api/v1/companyapp/'
+
+    def setUp(self) -> None:
+        self.admin = CustomUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        self.user = CustomUser.objects.create(username='company', email='company@mai.ru', password='qazwsx', role=3)
+
+    def test_get_list_company_card(self):
+        factory = APIRequestFactory()
+        request = factory.get(self.url)
+        force_authenticate(request, self.admin)
+        view = CompanyCardModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def tearDown(self) -> None:
+        pass
